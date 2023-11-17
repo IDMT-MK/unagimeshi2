3482107fdad8102147c091c95876a81c202e88e1
IDMT-MK <107912869+IDMT-MK@users.noreply.github.com>
2023-11-17 18:17:01 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,69 @@
+
+
+// Smooth Scrool /////////////////////////////////////////////////////////////
+
+$(function (){
+
+   // Smooth Scroll
+   $('a[href^="#"]').click(function() {
+
+      // スクロールの速度(ms)
+      var speed = 500;
+
+      // アンカーを取得
+      var anchor = $(this).attr("href");
+
+      // ターゲットの位置を取得
+      var target = $(anchor == "#" || anchor == "" ? 'html' : anchor);
+      var position = target.offset().top　-80;
+
+      // スクロール（アニメーション）
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+
+      return false;
+
+   });
+
+   $('.slider').slick({
+    fade:true,//切り替えをフェードで行う。初期値はfalse。
+    autoplay: true,//自動的に動き出すか。初期値はfalse。
+    autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
+    speed:1000,//スライドの動きのスピード。初期値は300。
+    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+    slidesToShow: 1,//スライドを画面に3枚見せる
+    slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+    arrows: true,//左右の矢印あり
+    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+    dots: true,//下部ドットナビゲーションの表示
+        pauseOnFocus: false,//フォーカスで一時停止を無効
+        pauseOnHover: false,//マウスホバーで一時停止を無効
+        pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
+    });
+  
+  //スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
+  $('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
+    $('.slider').slick('slickPlay');
+  });
+
+  $(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
+
+$('#page-top').click(function () {
+  $('body,html').animate({
+      scrollTop: 0//ページトップまでスクロール
+  }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+  return false;//リンク自体の無効化
+});
+
+});
+
+
+
